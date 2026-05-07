cfc01b5aece0c63497e0a475950deb1c0c310adc
Sachin Prajapati <sachinprajapati77249@gmail.com>
2026-05-08 02:30:06 +0530

    initial commit

--- assistant/chat_model.py
+++ assistant/chat_model.py
@@ -1,17 +1,14 @@
-from dotenv import load_dotenv
-load_dotenv()
 import os
 
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
     temperature=0,
-    max_tokens=100
+    max_tokens=100,
+    google_api_key=os.environ["GOOGLE_API_KEY"]
 )
-
 
 def res(query):
     messages = [
